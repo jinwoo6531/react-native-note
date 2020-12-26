78137515ab191c0083f04f0dd2a0f59644bdf663
jinwoo <jintakk6531@gmail.com>
2020-12-26 18:46:52 +0900

    12/26 note screen 추가완료

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -2,16 +2,21 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import ViewNotes from '../screens/ViewNotes';
+import AddNotes from '../screens/AddNotes';
 
 const StackNavigator = createStackNavigator(
   {
     ViewNotes: {
       screen: ViewNotes,
     },
+    AddNotes: {
+      screen: AddNotes,
+    },
   },
   {
     initialRouteName: 'ViewNotes',
     heaerMode: 'none',
+    mode: 'modal',
   }
 );
 
--- src/screens/ViewNotes.js
+++ src/screens/ViewNotes.js
@@ -1,34 +1,32 @@
-import React from 'react'
-import {StyleSheet,View} from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import { Text } from 'react-native-paper';
 
 const ViewNotes = () => {
-    return (
-        <View>
-            <View>
-                <Text>You do not have any Notes</Text>
-            </View>
-            
-        </View>
-    )
-}
-
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>You do not have any Notes</Text>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:'#fff',
-        paddingVertical: 20,
-        paddingHorizontal:10
-    },
-    titleContainer: {
-        alignItems: 'center',
-        justifyContent:'center',
-        flex:1
-    },
-    title: {
-        fontSize: 20
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+  },
+  titleContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+  },
+});
 
-export default ViewNotes
+export default ViewNotes;
